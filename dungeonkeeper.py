3e68975ce79214def62d9909e806421933f20f98
Ben <ben@mail.com>
2026-03-09 14:45:41 -0700

    dropping back to info logging level

--- dungeonkeeper.py
+++ dungeonkeeper.py
@@ -27,7 +27,7 @@ from xp_system import init_xp_tables
 load_dotenv()
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s %(levelname)-8s %(name)s %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     force=True,
